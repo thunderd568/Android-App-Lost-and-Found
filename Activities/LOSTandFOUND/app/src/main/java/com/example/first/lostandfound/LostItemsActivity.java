5f4813bea7665093ba4cbd37018e402f1a00ad34
peiyinchen <peiyin0922@gmail.com>
2018-11-05 16:28:47 -0500

    added activities

---
+++ Activities/LOSTandFOUND/app/src/main/java/com/example/first/lostandfound/LostItemsActivity.java
@@ -0,0 +1,13 @@
+package com.example.first.lostandfound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LostItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_items);
+    }
+}
