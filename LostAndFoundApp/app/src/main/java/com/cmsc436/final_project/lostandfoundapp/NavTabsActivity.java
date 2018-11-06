d94c672b2d75b9f3655e4a23e32d5d32441c437f
JDRamsey <31673223+jramsey3@users.noreply.github.com>
2018-11-05 23:44:33 -0500

    fragments added to main menu

--- LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/NavTabsActivity.java
+++ LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/NavTabsActivity.java
@@ -3,42 +3,56 @@ package com.cmsc436.final_project.lostandfoundapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class NavTabsActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class NavTabsActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_tabs);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new UserProfileFragment());
     }
 
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.tab_fragment_container, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_profile_tab:
+                fragment = new UserProfileFragment();
+                break;
+
+            case R.id.navigation_lost_tab:
+                fragment = new LostFragment();
+                break;
+
+            case R.id.navigation_found_tab:
+                fragment = new FoundFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
 }
