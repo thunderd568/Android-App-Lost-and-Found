35b2b0f9d610013137d9fef3561bcee1a74c8792
thunderd568 <nationals07@msn.com>
2018-11-27 19:45:08 -0500

    Basically replicated the lost fragment xml to look something like the Found fragment. Will revise.

--- LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/LostFragment.java
+++ LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/LostFragment.java
@@ -2,10 +2,22 @@ package com.cmsc436.final_project.lostandfoundapp;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,12 +25,49 @@ import android.view.ViewGroup;
  */
 public class LostFragment extends Fragment {
 
+    protected RecyclerView myReportsRecyclerView;
+    protected DatabaseReference databaseLostItems;
+    protected List<ItemReport> reports;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lost, container, false);
+        View fragView = inflater.inflate(R.layout.fragment_lost, container, false);
+
+        databaseLostItems = FirebaseDatabase.getInstance().getReference("lost_reports");
+
+        myReportsRecyclerView = (RecyclerView) fragView.findViewById(R.id.recyclerViewMyLostReports);
+        myReportsRecyclerView.setHasFixedSize(true);
+        myReportsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        reports = new ArrayList<ItemReport>();
+
+        return fragView;
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseLostItems.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                reports.clear();
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    ItemReport report = postSnapshot.getValue(ItemReport.class);
+                    reports.add(report);
+                }
+
+                ReportAdapter mAdapter = new ReportAdapter(getContext(), reports);
+                myReportsRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
