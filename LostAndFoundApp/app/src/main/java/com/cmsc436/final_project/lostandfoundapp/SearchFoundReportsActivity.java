ee480999023d7ad4ef0199a1ee188caf7eff4c6d
JDRamsey <31673223+jramsey3@users.noreply.github.com>
2018-12-01 19:53:28 -0500

    hmmmm

--- LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/SearchFoundReportsActivity.java
+++ LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/SearchFoundReportsActivity.java
@@ -3,12 +3,12 @@ package com.cmsc436.final_project.lostandfoundapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchLostReportsActivity extends AppCompatActivity {
+public class SearchFoundReportsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_lost_reports);
+        setContentView(R.layout.activity_search_found_reports);
     }
 
 }
