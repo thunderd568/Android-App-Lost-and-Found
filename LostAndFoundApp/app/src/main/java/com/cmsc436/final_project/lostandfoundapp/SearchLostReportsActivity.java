65668f8e55bb00736717cdc81796558fc8cc6893
JDRamsey <31673223+jramsey3@users.noreply.github.com>
2018-12-01 19:30:44 -0500

    started layout for searching found

---
+++ LostAndFoundApp/app/src/main/java/com/cmsc436/final_project/lostandfoundapp/SearchLostReportsActivity.java
@@ -0,0 +1,14 @@
+package com.cmsc436.final_project.lostandfoundapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchLostReportsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_lost_reports);
+    }
+
+}
